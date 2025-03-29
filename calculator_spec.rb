04a8302abcbf88bf64bb95dd6f0951783fe93c4c
SurendraGoud750 <160252688+SurendraGoud750@users.noreply.github.com>
2025-03-29 21:17:16 +0530

    Write RSpec tests for handling empty strings, a single number, and two numbers.

---
+++ calculator_spec.rb
@@ -0,0 +1,16 @@
+# spec/calculator_spec.rb
+require 'calculator'
+
+RSpec.describe 'Calculator' do
+  it 'returns 0 for an empty string' do
+    expect(add('')).to eq(0)
+  end
+
+  it 'returns the sum of a single number' do
+    expect(add('1')).to eq(1)
+  end
+
+  it 'returns the sum of two numbers' do
+    expect(add('1,2')).to eq(3)
+  end
+end
