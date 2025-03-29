07eaa274c08eb63dfa7833b557e902c193bb3c6b
SurendraGoud750 <160252688+SurendraGoud750@users.noreply.github.com>
2025-03-29 21:25:55 +0530

    Add an RSpec test for handling custom delimiters.

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -13,4 +13,12 @@ RSpec.describe 'Calculator' do
   it 'returns the sum of two numbers' do
     expect(add('1,2')).to eq(3)
   end
+  
+  it 'returns the sum of numbers separated by newlines' do
+    expect(add("1\n2\n3")).to eq(6)
+  end
+
+  it 'handles custom delimiter // and sums correctly' do
+    expect(add("//;\n1;2;3")).to eq(6)
+  end
 end
