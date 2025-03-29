e302e3f76bb302975bf31d6388eee455543cfeb0
SurendraGoud750 <160252688+SurendraGoud750@users.noreply.github.com>
2025-03-29 21:24:30 +0530

    Support Custom Delimiters

--- calcultor.rb
+++ calcultor.rb
@@ -2,6 +2,14 @@
 def add(numbers)
   return 0 if numbers.empty?
 
-  number_array = numbers.split(',')
+  if numbers.start_with?("//")
+    delimiter = numbers[2]
+    numbers = numbers.split("\n", 2).last
+  else
+    delimiter = ","
+  end
+
+  numbers = numbers.gsub("\n", delimiter)
+  number_array = numbers.split(delimiter)
   number_array.map(&:to_i).sum
 end
