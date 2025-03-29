9a242814b321ea7226cccf8b9c8726d5d22f4933
SurendraGoud750 <160252688+SurendraGoud750@users.noreply.github.com>
2025-03-29 21:32:05 +0530

    Support multiple custom delimiters like //[;][#].

--- calcultor.rb
+++ calcultor.rb
@@ -3,18 +3,22 @@ def add(numbers)
   return 0 if numbers.empty?
 
   if numbers.start_with?("//")
-    delimiter = numbers[2]
+    delimiters = numbers[2..-1].split('][').map { |d| d.gsub('[', '').gsub(']', '') }
     numbers = numbers.split("\n", 2).last
   else
-    delimiter = ","
+    delimiters = [","]
   end
 
-  numbers = numbers.gsub("\n", delimiter)
-  number_array = numbers.split(delimiter)
+  delimiters.each do |delimiter|
+    numbers = numbers.gsub(delimiter, ",")
+  end
+
+  number_array = numbers.split(',')
   numbers = number_array.map(&:to_i)
 
+  # Check for negative numbers
   negative_numbers = numbers.select { |n| n < 0 }
   raise "Negative numbers not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.empty?
-  
+
   numbers.sum
 end
